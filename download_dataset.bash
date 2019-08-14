1fc03a0406d6ed47367b6e875c747e7b9703d0bd
El Helou Majed <elhelou@iccluster143.iccluster.epfl.ch>
2019-08-14 18:30:32 +0200

    created code dir, get_dataset bash

---
+++ download_dataset.bash
@@ -0,0 +1,10 @@
+mkdir dataset
+wget http://data.vision.ee.ethz.ch/timofter/AIM19ExtremeSR/trainHR_001to200.zip -P dataset/
+wget http://data.vision.ee.ethz.ch/timofter/AIM19ExtremeSR/trainHR_201to400.zip -P dataset/
+wget http://data.vision.ee.ethz.ch/timofter/AIM19ExtremeSR/trainHR_401to600.zip -P dataset/
+wget http://data.vision.ee.ethz.ch/timofter/AIM19ExtremeSR/trainHR_601to800.zip -P dataset/
+wget http://data.vision.ee.ethz.ch/timofter/AIM19ExtremeSR/trainHR_801to1000.zip -P dataset/
+wget http://data.vision.ee.ethz.ch/timofter/AIM19ExtremeSR/trainHR_1001to1200.zip -P dataset/
+wget http://data.vision.ee.ethz.ch/timofter/AIM19ExtremeSR/trainHR_1201to1400.zip -P dataset/
+wget http://data.vision.ee.ethz.ch/timofter/AIM19ExtremeSR/trainHR_1401to1500.zip -P dataset/
+wget http://data.vision.ee.ethz.ch/timofter/AIM19ExtremeSR/validationLR.zip -P dataset/
