c791b431c3621235aeaff154bff3d3b552f75bdc
El Helou Majed <elhelou@iccluster143.iccluster.epfl.ch>
2019-08-14 18:55:20 +0200

    added bash for unzipping datasets

---
+++ unzip_dataset.bash
@@ -0,0 +1,14 @@
+cd dataset
+
+unzip validationLR.zip -d validationLR/
+
+unzip trainHR_001to200.zip -d trainHR/
+unzip trainHR_201to400.zip -d trainHR/
+unzip trainHR_401to600.zip -d trainHR/
+unzip trainHR_601to800.zip -d trainHR/
+unzip trainHR_801to1000.zip -d trainHR/
+unzip trainHR_1001to1200.zip -d trainHR/
+unzip trainHR_1201to1400.zip -d trainHR/
+unzip trainHR_1401to1500.zip -d trainHR/
+
+
